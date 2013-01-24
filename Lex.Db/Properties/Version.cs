2269ac09b8aaacd59ad94678d006ad3279b22dca
Lex Lavnikov <lex@lavnikov.com>
2013-01-24 17:17:38 +0100

    Release 1.1 (Improved Indexes)

--- Lex.Db/Properties/Version.cs
+++ Lex.Db/Properties/Version.cs
@@ -1,6 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lex.Db")]
+[assembly: AssemblyCopyright("Copyright © 2012-2013 Lex Lavnikov")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Lex.Db.Sync")]
+[assembly: ComVisible(false)]
+
